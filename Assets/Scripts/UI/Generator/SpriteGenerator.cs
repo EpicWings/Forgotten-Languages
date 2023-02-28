0c8a5681547854f179645b981162957175324c67
EpicWings <pasca_robert2003@yahoo.com>
2023-02-28 23:43:39 +0200

    Item Generator 1.1

--- Assets/Scripts/UI/Generator/SpriteGenerator.cs
+++ Assets/Scripts/UI/Generator/SpriteGenerator.cs
@@ -1,40 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class SpriteGenerator : MonoBehaviour
 {
-    [SerializeField] private Texture2D itemTexture;
-    [SerializeField] private Texture2D rarityTexture;
-    [SerializeField] private string path = "Assets/Data/Sprites";
+    [SerializeField] private Sprite[] itemSprites;
+    [SerializeField] private Sprite[] raritySprites;
+    
+    private readonly string savePathSprite = "Assets/Data/Sprites";
+    private readonly string savePathScriptable = "Assets/Data/Objects";
 
     private void Start()
     {
-        GenerateSprites();
-    }
-    private Sprite GenerateSprite(Texture2D itemTexture, Texture2D rarityTexture)
-    {
-        Sprite sprite = null;
-        var texture = new Texture2D(itemTexture.width, itemTexture.height);
-        var pixels = itemTexture.GetPixels();
-        var rarityPixels = rarityTexture.GetPixels();
-        for (int i = 0; i < pixels.Length; i++)
-        {
-            if (rarityPixels[i].a > 0)
-            {
-                pixels[i] = rarityPixels[i];
-            }
-        }
-        texture.SetPixels(pixels);
-        texture.Apply();
-        var bytes = texture.EncodeToPNG();
-        File.WriteAllBytes(path + "/item.png", bytes);
-        return sprite;
+        Merge();
+        ApplyChanges();
     }
 
-    private void GenerateSprites()
+
+    private void Merge()
     {
-        var sprite = GenerateSprite(itemTexture, rarityTexture);
+        Resources.UnloadUnusedAssets();
+        var texture = new Texture2D(32, 32);
+
+        for (int i = 0; i < texture.width; i++)
+            for (int j = 0; j < texture.height; j++)
+                texture.SetPixel(i, j, Color.clear);
+
+        int itemRandomIndex = Random.Range(0, itemSprites.Length);
+        int rarityRandomIndex = Random.Range(0, raritySprites.Length);
+
+        var itemSprite = itemSprites[itemRandomIndex];
+        var raritySprite = raritySprites[rarityRandomIndex];
+
+        for (int i = 0; i < raritySprite.texture.width; i++)
+            for (int j = 0; j < raritySprite.texture.height; j++)
+            {
+                var color = raritySprite.texture.GetPixel(i, j);
+                if (color.a > 0)
+                    texture.SetPixel(i, j, color);
+            }
+
+        for (int i = 0; i < itemSprite.texture.width; i++)
+            for (int j = 0; j < itemSprite.texture.height; j++)
+            {
+                var color = itemSprite.texture.GetPixel(i, j);
+                if (color.a > 0)
+                    texture.SetPixel(i, j, color);
+            }
+
+        texture.Apply();
+        var finalSprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero, 32);
+        finalSprite.name = itemSprite.name + "_" + raritySprite.name;
+
+        var bytes = finalSprite.texture.EncodeToPNG();
+        File.WriteAllBytes(savePathSprite + "/" + finalSprite.name + ".png", bytes);
     }
+
+    private void ApplyChanges()
+    {
+        foreach (var file in Directory.GetFiles(savePathSprite, "*.png").Where(file => !file.EndsWith(".meta")))
+        {
+            var importer = AssetImporter.GetAtPath(file) as TextureImporter;
+            importer.textureType = TextureImporterType.Sprite;
+            importer.spritePixelsPerUnit = 32;
+            importer.filterMode = FilterMode.Point;
+            importer.SaveAndReimport();
+        }
+        
+    }
+
+    private void CreateScriptableObject()
+    {
+        ItemScriptableObj itemScriptableObj;
+        itemScriptableObj = ScriptableObject.CreateInstance<ItemScriptableObj>();
+        itemScriptableObj.Name = "Test";
+
+        AssetDatabase.CreateAsset(itemScriptableObj, savePathScriptable + "/" + itemScriptableObj.Name + ".asset");
+    }
+
+    public void RemoveSprite()
+    {
+        //...
+    }
+
 }
